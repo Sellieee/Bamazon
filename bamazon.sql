592b337103242a2acc2dc03a32175f10d91fb6e1
Sellieee <sellieee@hotmail.com>
2019-09-01 18:11:39 +1000

    added 10 rows of inventory

--- bamazon.sql
+++ bamazon.sql
@@ -16,5 +16,16 @@ PRIMARY KEY
    (item_id)
 );
 
-   SELECT *
-   FROM products
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES 
+("Samsung Galaxy Note 10", "Mobile Phones", 1499 , 10), 
+("Nikon D5600", "DSLR", 849.95, 8),
+("Hikari Cichlid Gold Sinking Pellets 342g", "Pet Food", 18.49, 20),
+("Nintendo Switch", "Gaming", 449, 10),
+("Asus Zenbook i5", "Laptops", 1599, 7),
+("Google Home", "Smart Home", 144, 30),
+("Logitech G915 Gaming Keyboard", "Gaming", 399, 15),
+("Logitech G502 Wireless Gaming Mouse", "Gaming", 249, 22),
+("Hikari Algae Wafers 82g", "Pet Food", 33.99, 12),
+("Apple iPhone XS Max", "Mobile Phones", 1799, 25);
+
+   SELECT * FROM products
